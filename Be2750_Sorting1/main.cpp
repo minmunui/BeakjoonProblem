c15f84019dcfb225bf033acc96a330dbb8b4498d
minmunui <ehdwls1638@naver.com>
2022-07-11 16:41:15 +0900

    2750 complete

--- Be2750_Sorting1/main.cpp
+++ Be2750_Sorting1/main.cpp
@@ -1,18 +1,23 @@
 #include "iostream"
 #include "vector"
+#include <queue>
 
 using namespace std;
 
 int main() {
     int n = 0;
     cin >> n;
-    
-    vector<int> v;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
 
     for (int i = 0 ; i < n ; i ++ ) {
         int input;
         cin >> input;
-        v.push_back(input);
+        pq.push(input);
+    }
+
+    while(!pq.empty()) {
+        cout << pq.top() << endl;
+        pq.pop();
     }
 }
-
