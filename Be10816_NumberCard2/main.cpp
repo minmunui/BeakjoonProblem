7267288848a9c553d1385d6da131476506da5cea
minmunui <ehdwls1638@gmail.com>
2022-07-14 19:12:36 +0900

    10816 Using Array Complete

--- Be10816_NumberCard2/main.cpp
+++ Be10816_NumberCard2/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  vector<int> vec;
+
+  int input = 0;
+  for ( int i = 0 ; i < n ; i ++ ) {
+    cin >> input;
+    vec.push_back(input);
+  }
+
+  sort(vec.begin(), vec.end());
+
+  int m;
+  cin >> m;
+  for ( int i = 0 ; i < m ; i ++ ) {
+    cin >> input;
+    auto lowerIter = lower_bound(vec.begin(), vec.end(), input);
+    auto upperIter = lowerIter;
+    if ( *lowerIter == input ) {
+      while (*upperIter == *lowerIter) {
+        upperIter++;
+      }
+      cout << upperIter - lowerIter << " ";
+    } else {
+      cout << 0 << " ";
+    }
+  }
 }
