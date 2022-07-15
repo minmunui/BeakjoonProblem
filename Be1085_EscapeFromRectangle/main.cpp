c54866a08d713a6e12923544bf8ef7a848fd8da8
minmunui <ehdwls1638@gmail.com>
2022-07-15 22:13:31 +0900

    1085 Progress...

---
+++ Be1085_EscapeFromRectangle/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int x, y, w, h;
+
+  cin >> x >> y >> w >> h;
+
+  result = 0;
+  if ( 0 <= x and x <= w ) {
+    min()
+  }
+
+  return 0;
+}
